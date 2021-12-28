8ab3d3d16ff1d49922b5b5034989f56a3aef75d3
28188 <2818815189@qq.com>
2021-12-28 23:35:39 +0800

    增加算法案例

---
+++ src/com/rowoffer/SubarraySum.java
@@ -0,0 +1,30 @@
+package com.rowoffer;
+
+public class SubarraySum {
+    public int subarraySum(int[] nums, int k) {
+        int sum[] = new int[nums.length+1];
+        for (int i = 1; i <= nums.length; i++) {
+            sum[i] = sum[i-1] + nums[i - 1];
+        }
+
+
+        int count = 0;
+        for (int i = 0; i <= sum.length; i++) {
+//            if (sum[i] == k) {
+//                count++;
+//            }
+            for (int i1 = 1; i1 < i; i1++) {
+                if (sum[i] - sum[i1] == k) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        SubarraySum subarraySum = new SubarraySum();
+        int arr[] = {1,1,1};
+        subarraySum.subarraySum(arr,2);
+    }
+}
