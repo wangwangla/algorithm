f8161035f7dc72d8ed925eabf38abfd5c9c21256
28188 <2818815189@qq.com>
2022-01-29 08:48:54 +0800

    复习

--- src/com/labuladong/chapterone/_112_Traverse.java
+++ src/com/labuladong/chapterone/_112_Traverse.java
@@ -1,6 +1,9 @@
 package com.labuladong.chapterone;
 
 import com.algorithm.binaryTree.ListNode;
+import com.algorithm.binaryTree.TreeNode;
+
+import java.util.LinkedList;
 
 public class _112_Traverse {
     private void traverse1(int arr[]){
@@ -8,6 +11,30 @@ public class _112_Traverse {
             System.out.println(arr[i]);
         }
     }
+
+    private void travese2(ListNode head){
+        for (ListNode node = head;node!=null;node= node.next){
+            System.out.println(node.val);
+        }
+    }
+
+    private void traverse2(ListNode head){
+        if (head==null){
+            return;
+        }
+
+        travese2(head.next);
+    }
+
+    public int ans = Integer.MIN_VALUE;
+    int oneSideMax(TreeNode root){
+        if (root == null)return 0;
+        int left = Math.max(0,oneSideMax(root.left));
+        int right = Math.max(0,oneSideMax(root.right));
+        return Math.max(left,right)+root.val;
+    }
+
+
 
     private void traverse1(ListNode head){
         while (head!=null){
@@ -18,4 +45,33 @@ public class _112_Traverse {
             System.out.println(head.val);
         }
     }
+
+    public void br(int arr[], LinkedList<Integer> track){
+        if (track.size() == arr.length){
+            System.out.println("----");
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (track.contains(arr[i])){
+                continue;
+            }
+            track.add(arr[i]);
+            br(arr,track);
+            track.removeLast();
+        }
+    }
+
+    public void br(int arr[], int v[] ,LinkedList<Integer> track){
+        if (track.size() == arr.length){
+            System.out.println("----");
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (v[i] == 1)continue;
+            v[i] = 1;
+            track.add(arr[i]);
+            br(arr,track);
+            track.removeLast();
+            v[i] = 1;
+        }
+    }
+
 }
