1e660ce7aec982ff3b91103e23248ac9ab353ccf
28188 <2818815189@qq.com>
2022-06-02 09:29:56 +0800

    上传

--- src/com/zhuanti/lianbiao/MST0201RemoveDuplicateNodes.java
+++ src/com/zhuanti/lianbiao/MST0201RemoveDuplicateNodes.java
@@ -5,19 +5,23 @@ import com.zhuanti.binaryTree.ListNode;
 import java.util.HashSet;
 
 public class MST0201RemoveDuplicateNodes {
-    public ListNode removeDuplicateNodes(ListNode head) {
-        ListNode newHead = head;
-        ListNode temp = head;
-        HashSet<Integer> hashSet = new HashSet<>();
-        while (temp!=null){
-            hashSet.add(temp.val);
-        }
-        temp = head;
-        for (Integer integer : hashSet) {
-            temp.val = integer;
-            temp = temp.next;
-        }
-        temp.next = null;
-        return head;
-    }
+
+//    public ListNode removeDuplicateNodes(ListNode head) {
+//        ListNode newHead = head;
+//        ListNode temp = head;
+//        HashSet<Integer> hashSet = new HashSet<>();
+//        while (temp!=null){
+//            if(!hashSet.contains(temp.val)){
+//                hashSet.add(temp.val);
+//                if (newHead==null){
+//                    newHead = temp;
+//                }else {
+//                    newHead.next = temp;
+//                    newHead = newHead.next;
+//                }
+//            }
+//            temp = temp.next;
+//        }
+//        return head;
+//    }
 }
