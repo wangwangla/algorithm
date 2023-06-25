463b0be1ac9008b3246b48c202250022dbf6d899
wangwangla <2818815189@qq.com>
2023-06-26 07:57:44 +0800

    算法

---
+++ src/kw/java/string/ValidParentheses.java
@@ -0,0 +1,33 @@
+package kw.java.string;
+
+public class ValidParentheses {
+    public static boolean isValid(String s) {
+        char ch[] = new char[s.length()];
+        int head = 0 ;
+        for (char c : s.toCharArray()) {
+            switch (c) {
+                case '(':
+                case '{':
+                case '[':
+                    ch[head++] = c;
+                    break;
+                case ')':
+                    if (head == 0 || ch[head] != '(') {
+                        return false;
+                    }
+                    break;
+                case ']':
+                    if (head == 0 || ch[head] != '(') {
+                        return false;
+                    }
+                    break;
+                case '}':
+                    if (head == 0 || ch[head] != '(') {
+                        return false;
+                    }
+                    break;
+            }
+        }
+        return head == 0;
+    }
+}
