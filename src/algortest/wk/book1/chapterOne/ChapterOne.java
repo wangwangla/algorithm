b3dcde2e27e24d2f71f4c2b883c2327fa53912c4
kangwang <2818815189@qq.com>
2024-02-27 08:36:05 +0800

    看看

--- src/algortest/wk/book1/chapterOne/ChapterOne.java
+++ src/algortest/wk/book1/chapterOne/ChapterOne.java
@@ -6,21 +6,29 @@ package algortest.wk.book1.chapterOne;
  */
 public class ChapterOne {
     public void test(){
-        int numPeople = 3;
+        int numPeople = 4;
         int numDeng = 14;
         boolean arr[] = new boolean[numDeng+1];
+        /**
+         * 开始灯都是灭的
+         */
         for (int i = 0; i < numDeng; i++) {
             arr[i] = false;
         }
-        for (int i = 1; i <= numPeople; i++) {
+        /**
+         * 每个人过来将灯取反
+         */
+        for (int i = 2; i <= numPeople; i++) {
             for (int i1 = 1; i1 <= numDeng; i1++) {
                 if (i1 % i == 0){
                     arr[i1] = !arr[i1];
                 }
             }
         }
-
-        for (int i = 0; i < numDeng; i++) {
+        /**
+         * 最后那个灯亮着
+         */
+        for (int i = 1; i <= numDeng; i++) {
             if (arr[i]) {
                 System.out.println(i+" ");
             }
@@ -29,5 +37,24 @@ public class ChapterOne {
     public static void main(String[] args) {
         ChapterOne chapterOne = new ChapterOne();
         chapterOne.test();
+    }
+
+    /**
+     * 灯的位置
+     */
+    public void test1(){
+        int numPeople = 4;
+        int numDeng = 14;
+        boolean arr[] = new boolean[numDeng+1];
+        for (int i = 1; i <= numDeng; i++) {
+            for (int i1 = 1; i1 < numPeople; i1++) {
+                int i2 = i % i1;
+                if (i2 == 0){
+                    //开始取反   最后的值
+
+                    //人数的倍数
+                }
+            }
+        }
     }
 }
